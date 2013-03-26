bc150a84a83d2399f22307355e69b8b085391468
Jurgen Blignaut <jurgen@webtide.co.za>
2013-03-26 18:26:07 +0200

    Implemented the basic create/subscribe/unsubscribe/delete interface with EverLytic

--- everlytic/api.py
+++ everlytic/api.py
@@ -1,4 +1,5 @@
 from xmlrpclib import ServerProxy, Fault, Error, ProtocolError
+from xml.parsers.expat import ExpatError
 
 from django.core.exceptions import ImproperlyConfigured
 from django.conf import settings
@@ -14,14 +15,104 @@ except KeyError as e:
     raise ImproperlyConfigured("EVERLYTIC setting %s is missing." % str(e))
 
 
-def subscribeUser(user):
+def subscribeUser(last_name, first_name, 
+                  email, receive_email,
+                  everlytic_id):
     """ Subscribe the user to the everlytic mailing list, and return the
     everlytic user id to the caller.
     """
-    return 200
+    # create the connection to the host
+    sp = None
+    try:
+        sp = ServerProxy(EVERLYTIC_HOST)
+    except IOError:
+        return -1
+
+    # Check for an existing everlytic user just to be safe
+    if everlytic_id == 0:
+        everlytic_id = _checkForExistingEverlyticUser(last_name,
+                                                     first_name,
+                                                     email, sp)
+
+    if everlytic_id < 0:
+        # We have a new Everlytic user, so create the user on the everlytic
+        # database, even if they don't subscribe to mailing lists yet.
+        return _createEverlyticUser(sp, email, receive_email,
+                                    last_name, first_name)
+
+    # For an existing user, update her status on the list
+    return _createEverlyticUser(sp, email, receive_email)
 
 
-def unsubscribeUser(user):
-    """ unsubscribe the user from the Everlytic mailing list.
+def _createEverlyticUser(sp, email, receive_email, 
+                         last_name=None, first_name=None):
+    """ Create a new user on the EverLytic service and (un)subscribe them to
+    the list, all in one go.
     """
-    pass
+    params = {
+            'contact_email': email
+            }
+    if first_name is not None:
+        params['contact_name'] = first_name
+    if last_name is not None:
+        params['contact_lastname'] = last_name
+    try:
+        result = sp.contacts.create(EVERLYTIC_API_KEY, 
+                params, 
+                [EVERLYTIC_LIST_ID], 
+                receive_email and "subscribed" or "unsubscribed", 
+                "update")
+        if result['status'] == 'success':
+            return result['contact_id']
+        else:
+            # TODO: Log the result['message']
+            pass
+    except Fault:
+        # TODO: Log the error
+        pass
+    return -1
+
+
+def _checkForExistingEverlyticUser(last_name, first_name, email, sp):
+    """ Check if the user exists on the Everlytic database
+    """
+    try:
+        # see if the user exists on the EverLytic service
+        result = sp.contacts.getBatch(EVERLYTIC_API_KEY, {
+            'contact_lastname': last_name,
+            'contact_name': first_name,
+            'contact_email': email
+            })
+        if int(result['total']) > 0:
+            # TODO: Make sure this compromise will work!
+            # Take the first result in the returned list
+            everlytic_user = result['data'][0]
+            return everlytic_user['contact_id']
+    except Fault:
+        # TODO: Log the error
+        pass
+    except ExpatError:
+        pass
+    return -1
+
+
+def deleteEverlyticUser(contact_id):
+    """ Delete the given contact from the Everlytic database
+    """
+    # create the connection to the host
+    sp = None
+    try:
+        sp = ServerProxy(EVERLYTIC_HOST)
+    except IOError:
+        # TODO: Log the error
+        return
+    try:
+        result = sp.contacts.delete(EVERLYTIC_API_KEY, contact_id)
+        if result['status'] != 'success':
+            # TODO: LOG the result['message']
+            pass
+    except Fault:
+        # TODO: Log the error
+        pass
+    except ExpatError:
+        pass
--- everlytic/models.py
+++ everlytic/models.py
@@ -3,28 +3,36 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
+from foundry.models import Member
+
 import api
 
 
 class EverlyticProfile(models.Model):
     user = models.OneToOneField(User)
-    # the Neo consumer id used in API requests
-    everlytic_id = models.PositiveIntegerField()
+    # the Everlytic contact_id used in API requests
+    everlytic_id = models.PositiveIntegerField(default=0)
 
     def __unicode__(self):
         return self.user.__unicode__()
 
 
-@receiver(post_save, sender=User)
-def user_post_save(sender, instance, created, **kwargs):
-    """ Create an EverlyticProfile and subscribe to the appropriate mailing
-    list on the Everlytic service. Store the Everlytic user id in the
-    profile.
+@receiver(post_save, sender=Member)
+def user_post_save(sender, instance, **kwargs):
+    """ Create an EverlyticProfile and store the Everlytic contact_id in
+    the profile. (Use it to delete the Contact record).
+    Create the Everlytic Contact record if it does not exist.  
+    Set the subscription status of the Contact on the Everlytic mailing
+    list.
     """
-    if created and not EverlyticProfile.objects.filter(user=instance).exists():
-        ep = EverlyticProfile.objects.create(user=instance, everlytic_id=0)
-        ep.everlytic_id = api.subscribeUser(instance)
-        ep.save()
+    ep, created = EverlyticProfile.objects.get_or_create(user=instance)
+    if instance.email:
+        ep.everlytic_id = api.subscribeUser(instance.last_name, 
+                                            instance.first_name,
+                                            instance.email,
+                                            instance.receive_email,
+                                            ep.everlytic_id)
+    ep.save()
 
 
 @receiver(pre_delete, sender=User)
@@ -35,7 +43,7 @@ def user_pre_delete(sender, instance, **kwargs):
     """
     try:
         ep = EverlyticProfile.objects.get(user=instance)
-        api.unsubscribeUser(ep.everlytic_id)
+        api.deleteEverlyticUser(ep.everlytic_id)
         # ep.delete()
     except EverlyticProfile.DoesNotExist:
         pass
