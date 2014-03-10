70e7adc17e533bb90b9204be7fab415b79bdecb4
Jason Paidoussi <jason@paidoussi.net>
2014-03-10 11:45:05 +0200

    Added celery retry on everlytic api failure

--- everlytic/tasks.py
+++ everlytic/tasks.py
@@ -5,17 +5,20 @@ from foundry.models import Member
 from everlytic import api
 
 
-@task
+@task(bind=True)
 def subscribe_user(member_id, profile_class):
     instance = Member.objects.get(pk=member_id)
     ep, created = profile_class.objects.get_or_create(member=instance)
     if created and instance.email:
-        ep.everlytic_id = api.subscribeUser(
+        everlytic_id = api.subscribeUser(
             instance.last_name,
             instance.first_name,
             instance.email,
             instance.receive_email,
             everlytic_id=ep.everlytic_id)
+        if everlytic_id is None:
+            raise self.retry()
+        ep.everlytic_id = everlytic_id
         ep.save()
 
 
